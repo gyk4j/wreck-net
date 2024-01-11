e51b70a0cff5575dbde6498abd1ce8e142f96797
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-01-11 11:22:33 +0800

    Add Wreck.IO.Reader namespace core files

---
+++ Wreck/IO/Reader/AbstractTimestampReader.cs
@@ -0,0 +1,128 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+using Wreck.Resources;
+
+namespace Wreck.IO.Reader
+{
+	/// <summary>
+	/// Description of AbstractTimestampReader.
+	/// </summary>
+	public abstract class AbstractTimestampReader : ITimestampReader
+	{
+		private static readonly string[] NONE = new string[0];
+		
+		private readonly Dictionary<string, CorrectionEnum> keys;
+		
+		public AbstractTimestampReader()
+		{
+			keys = new Dictionary<string, CorrectionEnum>();
+			
+			foreach(string s in creation())
+			{
+				keys.Add(s, CorrectionEnum.CREATION);
+			}
+			
+			foreach(string s in modified())
+			{
+				keys.Add(s, CorrectionEnum.MODIFIED);
+			}
+			
+			foreach(string s in accessed())
+			{
+				keys.Add(s, CorrectionEnum.ACCESSED);
+			}
+		}
+		
+		public void extract(FileSystemInfo file, List<Metadata> metadata)
+		{
+			throw new NotImplementedException();
+		}
+		
+		public string[] creation()
+		{
+			return NONE;
+		}
+		
+		public string[] modified()
+		{
+			return NONE;
+		}
+		
+		public string[] accessed()
+		{
+			return NONE;
+		}
+		
+		protected Dictionary<string, CorrectionEnum> GetKeys()
+		{
+			return keys;
+		}
+		
+		/// <summary>
+		/// Add a <c>System.DateTime</c> value.
+		/// </summary>
+		/// <param name="metadata"></param>
+		/// <param name="key"></param>
+		/// <param name="val"></param>
+		/// <param name="time"></param>
+		protected void Add(
+			List<Metadata> metadata,
+			string key,
+			string val,
+			DateTime time)
+		{
+			CorrectionEnum ce = null;
+			bool exists = GetKeys().TryGetValue(key, out ce);
+			
+			Metadata m = new Metadata(
+				key,
+				val,
+				time,
+				ce);
+			metadata.Add(m);
+		}
+		
+		/// <summary>
+		/// Add a File Time represented as a <c>long</c> value.
+		/// </summary>
+		/// <param name="metadata"></param>
+		/// <param name="key"></param>
+		/// <param name="val"></param>
+		/// <param name="time"></param>
+		protected void Add(
+			List<Metadata> metadata,
+			string key,
+			string val,
+			long time)
+		{
+			DateTime ft = DateTime.FromFileTimeUtc(time);
+			Add(metadata, key, val, ft);
+		}
+		
+		/// <summary>
+		/// Add a ISO-8601 formatted string date time.
+		/// </summary>
+		/// <param name="metadata"></param>
+		/// <param name="key"></param>
+		/// <param name="val"></param>
+		/// <param name="time"></param>
+		protected void Add(
+			List<Metadata> metadata,
+			string key,
+			string val,
+			string time)
+		{
+			DateTime it = DateTime.Parse(time, null, DateTimeStyles.RoundtripKind);
+			Add(metadata, key, val, it);
+		}
+		
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Wreck/IO/Reader/ReaderFactory.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+
+namespace Wreck.IO.Reader
+{
+	/// <summary>
+	/// Description of ReaderFactory.
+	/// </summary>
+	public class ReaderFactory : IDisposable
+	{
+		private static ReaderFactory instance = null;
+		
+//		private final ExifToolReader exifToolReader;
+//		private final MediaInfoReader mediaInfoReader;
+//		private final SevenZipReader sevenZipReader;
+//		private final PathReader pathReader;
+//		
+//		private final FileSystemReader fileSystemReader;
+//		private final CustomDateTimeReader customDateTimeReader;
+		
+		public static bool IsInitialized()
+		{
+			return instance != null;
+		}
+	
+		private ReaderFactory()
+		{
+			// Primary embedded metadata readers
+//			exifToolReader = new ExifToolReader();
+//			mediaInfoReader = new MediaInfoReader();
+//			sevenZipReader = new SevenZipReader();
+//			pathReader = new PathReader();
+			
+			// Secondary backup readers
+//			fileSystemReader = new FileSystemReader();
+//			customDateTimeReader = new CustomDateTimeReader();
+		}
+		
+		public static ReaderFactory Instance
+		{
+			get
+			{
+				if(instance == null)
+				{
+					instance = new ReaderFactory();
+				}
+				return instance;
+			}
+		}
+		
+		public void Dispose()
+		{
+//			exifToolReader.Dispose();
+//			mediaInfoReader.Dispose();
+//			sevenZipReader.Dispose();
+//			pathReader.Dispose();
+//			
+//			fileSystemReader.Dispose();
+//			customDateTimeReader.Dispose();
+		}
+	}
+}
