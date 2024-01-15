0a8e506b0eefc95f40ebbd6f274668a8fe1a5627
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-01-15 09:11:19 +0800

    Add AnalyzeWriter

---
+++ Wreck/IO/Writer/AnalyzeWriter.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Wreck.Resources;
+using Wreck.Util.Logging;
+
+namespace Wreck.IO.Writer
+{
+	/// <summary>
+	/// Description of AnalyzeWriter.
+	/// </summary>
+	public class AnalyzeWriter : AbstractTimestampWriter
+	{
+		public override void Write(
+			FileSystemInfo file,
+			Dictionary<CorrectionEnum, DateTime> values)
+		{
+			
+			WriteAttribute(
+				FileEvent.CorrectibleCreation,
+				FileEvent.UncorrectibleCreation,
+				file.CreationTime,
+				values[CorrectionEnum.CREATION]);
+			
+			WriteAttribute(
+				FileEvent.CorrectibleModified,
+				FileEvent.UncorrectibleModified,
+				file.LastWriteTime,
+				values[CorrectionEnum.MODIFIED]);
+			
+			WriteAttribute(
+				FileEvent.CorrectibleAccessed,
+				FileEvent.UncorrectibleAccessed,
+				file.LastAccessTime,
+				values[CorrectionEnum.ACCESSED]);
+		}
+	}
+}
