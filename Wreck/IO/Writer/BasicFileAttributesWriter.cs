ff2abc81828d811c32b8244323984af262126ae7
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-01-15 08:55:57 +0800

    Add BasicFileAttributesWriter

---
+++ Wreck/IO/Writer/BasicFileAttributesWriter.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using log4net;
+using Wreck.Resources;
+using Wreck.Util.Logging;
+
+namespace Wreck.IO.Writer
+{
+	/// <summary>
+	/// Description of BasicFileAttributesWriter.
+	/// </summary>
+	public class BasicFileAttributesWriter : AbstractTimestampWriter
+	{
+		private static readonly ILog LOG = LogManager.GetLogger(typeof(BasicFileAttributesWriter));
+		/*
+		public BasicFileAttributesWriter()
+		{
+		}
+		 */
+		
+		public override void Write(
+			FileSystemInfo file,
+			Dictionary<CorrectionEnum, DateTime> values)
+		{
+			
+			DateTime creation = WriteAttribute(
+				FileEvent.CorrectibleCreation,
+				FileEvent.UncorrectibleCreation,
+				file.CreationTime,
+				values[CorrectionEnum.CREATION]);
+			
+			DateTime modified = WriteAttribute(
+				FileEvent.CorrectibleModified,
+				FileEvent.UncorrectibleModified,
+				file.LastWriteTime,
+				values[CorrectionEnum.MODIFIED]);
+			
+			DateTime accessed = WriteAttribute(
+				FileEvent.CorrectibleAccessed,
+				FileEvent.UncorrectibleAccessed,
+				file.LastAccessTime,
+				values[CorrectionEnum.ACCESSED]);
+			
+			if(creation != null)
+				file.CreationTime = creation;
+			
+			if(modified != null)
+				file.LastWriteTime = modified;
+			
+			if(accessed != null)
+				file.LastAccessTime = accessed;
+			
+			LOG.InfoFormat("Updated {0}: {1}, {2}, {3}",
+			         file.FullName,
+			         creation,
+			         modified,
+			         accessed);
+		}
+	}
+}
---
+++ Wreck/Util/Logging/FileEvent.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace Wreck.Util.Logging
+{
+	/// <summary>
+	/// Description of FileEvent.
+	/// </summary>
+	public enum FileEvent
+	{
+		DirectoryFound,
+		FileFound,
+		FileError,
+		CorrectibleCreation,
+		CorrectibleModified,
+		CorrectibleAccessed,
+		UncorrectibleCreation,
+		UncorrectibleModified,
+		UncorrectibleAccessed
+	}
+}
