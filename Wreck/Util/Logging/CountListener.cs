87eaf84b27bf3ed1398d572aaab0ff002aecd1ba
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-01-15 12:04:46 +0800

    Add more logging files

---
+++ Wreck/Util/Logging/CountListener.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Wreck.Util.Logging
+{
+	/// <summary>
+	/// Description of CountListener.
+	/// </summary>
+	public class CountListener<T> : StatisticsListener<T, int>
+	{
+		private readonly IDictionary<T, int> statistics = new Dictionary<T, int>();
+		
+		public IDictionary<T, int> Statistics
+		{
+		
+			get { return statistics; }
+		}
+		
+		public IDictionary<T, int> Get()
+		{
+			return Statistics;
+		}
+
+		public int Get(T t)
+		{
+			int val;
+			bool ok = Statistics.TryGetValue(t, out val);
+			return ok ? val : 0;
+		}
+
+		public int Count(T t)
+		{
+			int v = Get(t);
+			statistics[t] = ++v;
+			return v;
+		}
+
+		public void Clear()
+		{
+			Statistics.Clear();
+		}
+	}
+}
---
+++ Wreck/Util/Logging/StatisticsListener.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Wreck.Util.Logging
+{
+	/// <summary>
+	/// Description of StatisticsListener.
+	/// </summary>
+	public interface StatisticsListener<T, U>
+	{
+		IDictionary<T, U> Get();
+		U Get(T t);
+		U Count(T t);
+		void Clear();
+	}
+}
