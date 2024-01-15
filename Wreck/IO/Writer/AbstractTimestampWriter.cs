409ed37346178894e6528aa22c0efacb3cd74d04
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-01-15 08:58:07 +0800

    Fix missing AbstractTimestampWriter

---
+++ Wreck/IO/Writer/AbstractTimestampWriter.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using log4net;
+using Wreck.Resources;
+using Wreck.Time;
+using Wreck.Util.Logging;
+
+namespace Wreck.IO.Writer
+{
+	/// <summary>
+	/// Description of AbstractTimestampWriter.
+	/// </summary>
+	public abstract class AbstractTimestampWriter : ITimestampWriter
+	{
+		private static readonly ILog LOG = LogManager.GetLogger(typeof(AbstractTimestampWriter));
+//		private static readonly StatisticsCollector STATS = StatisticsCollector.getInstance();
+		
+		protected DateTime WriteAttribute(
+			FileEvent required,
+			FileEvent none,
+			DateTime fileSystem,
+			DateTime metadata)
+		{
+			DateTime? val = null;
+			
+			DateTime correct = metadata;
+			
+			if (correct == null)
+			{
+//				STATS.count(none);
+			}
+			else if(TimeUtils.IsLaterThan(fileSystem, correct))
+			{
+//				STATS.count(required);
+				val = correct;
+			}
+			return val.Value;
+		}
+		
+		protected DateTime RestoreAttribute(
+			FileEvent required,
+			FileEvent none,
+			DateTime fileSystem,
+			DateTime metadata)
+		{
+			
+			if (metadata == null)
+			{
+//				STATS.count(none);
+				LOG.Warn("Missing correct value");
+			}
+			else if(!TimeUtils.IsSameTime(fileSystem, metadata))
+			{
+//				STATS.count(required);
+			}
+			
+			return metadata;
+		}
+		
+		public abstract void Write(FileSystemInfo file, Dictionary<CorrectionEnum, DateTime> values);
+	}
+}
