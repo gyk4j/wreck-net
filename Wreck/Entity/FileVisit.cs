bdaeb6e2cedc0317230c31cab7b84a8d73df0a7f
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-01-17 11:21:44 +0800

    Add PreviewService and ProgressWorker

---
+++ Wreck/Entity/FileVisit.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.IO;
+
+namespace Wreck.Entity
+{
+	/// <summary>
+	/// Description of FileVisit.
+	/// </summary>
+	public class FileVisit
+	{
+		private FileSystemInfo file;
+		private int progress;
+		
+		public FileVisit(FileSystemInfo file)
+		{
+			this.file = file;
+		}
+		
+		public FileSystemInfo File
+		{
+			get { return file; }
+			set { File = value; }
+		}
+		
+		public int Progress
+		{
+			get { return progress; }
+			set { progress = value; }
+		}
+	}
+}
