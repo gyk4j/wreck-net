e4d1c705e9454cd07af97132470547abe7053469
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-01-18 11:45:44 +0800

    Fix missing types build errors

--- JShim/NIO/File/FileStore.cs
+++ JShim/NIO/File/FileStore.cs
@@ -13,13 +13,13 @@ namespace JShim.NIO.File
 		}
 		
 		public abstract object GetAttribute(string attribute);
-		public abstract V GetFileStoreAttributeView<V>(Type type) where V : FileStoreAttributeView;
+//		public abstract V GetFileStoreAttributeView<V>(Type type) where V : FileStoreAttributeView;
 		public abstract long GetTotalSpace();
 		public abstract long GetUnallocatedSpace();
 		public abstract long GetUsableSpace();
 		public abstract bool IsReadOnly();
 		public abstract string	Name();
-		public abstract bool SupportsFileAttributeView<T>(Type type) where T : FileAttributeView;
+//		public abstract bool SupportsFileAttributeView<T>(Type type) where T : FileAttributeView;
 		public abstract bool SupportsFileAttributeView(string name);
 		public abstract string	Type();
 	}
