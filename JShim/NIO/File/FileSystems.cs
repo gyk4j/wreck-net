048d586dcc455de3ad4ce39137fbce234c8277c6
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-01-19 12:11:48 +0800

    Add FileSystem GetPath implementation

---
+++ JShim/NIO/File/FileSystems.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using Sun.NIO.FS;
+
+namespace JShim.NIO.File
+{
+	/// <summary>
+	/// Description of FileSystems.
+	/// </summary>
+	public class FileSystems
+	{
+		protected static readonly FileSystem defaultFileSystem = new WindowsFileSystem();
+		
+		public static FileSystem GetDefault()
+		{
+			return defaultFileSystem;
+		}
+	}
+}
