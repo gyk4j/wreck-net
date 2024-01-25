d19735648520d28927349d7335a4170737090b69
gyk <147011991+gyk4j@users.noreply.github.com>
2024-01-25 15:51:39 +0800

    Add SwingWorker skeleton stub (#110)

---
+++ JShim/Beans/PropertyChangeSupport.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+namespace JShim.Beans
+{
+	/// <summary>
+	/// Description of PropertyChangeSupport.
+	/// </summary>
+	public class PropertyChangeSupport
+	{
+		public PropertyChangeSupport()
+		{
+		}
+	}
+}
---
+++ JShim/Util/Concurrent/TimeUnit.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace JShim.Util.Concurrent
+{
+	/// <summary>
+	/// Description of TimeUnit.
+	/// </summary>
+	public enum TimeUnit
+	{
+		Days,
+		Hours,
+		Microseconds,
+		Milliseconds,
+		Minutes,
+		Nanoseconds,
+		Seconds
+	}
+}
