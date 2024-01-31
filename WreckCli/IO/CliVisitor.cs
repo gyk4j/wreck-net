ad58476dfe4e8238241d518f2ed0f9b2e01072f1
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-01-31 14:38:29 +0800

    Add CliWorker override methods

--- WreckCli/IO/CliVisitor.cs
+++ WreckCli/IO/CliVisitor.cs
@@ -33,5 +33,61 @@ namespace Wreck.IO
 		
 		public List<DirectoryInfo> Directories { get { return directories; } }
 		public Dictionary<CorrectionEnum, DateTime> Suggestions { get { return suggestions; } }
+	
+		public override FileVisitResult PreVisitDirectory(DirectoryInfo dir)
+		{
+			STATS.Count(FileEvent.DirectoryFound);
+			STATS.Count(FileEvent.FileFound);
+			
+			Directories.Add(dir);
+			
+			Suggestions.Clear();
+			progressWorker.Task.PreVisitDirectory(Suggestions, dir);
+			
+			return FileVisitResult.Continue;
+		}
+		
+		public override FileVisitResult PostVisitDirectory(DirectoryInfo dir, IOException exc)
+		{			
+			if(exc != null)
+			{
+				LOG.Error(exc.ToString());
+				return FileVisitResult.Continue;
+			}
+			
+			bool ok = Directories.Remove(dir);
+			
+			if(!ok)
+				LOG.WarnFormat("{0}: attrs = null", dir.FullName);
+			
+			Suggestions.Clear();
+			progressWorker.Task.PostVisitDirectory(Suggestions, dir);
+			
+			return FileVisitResult.Continue;
+		}
+		
+		public override FileVisitResult VisitFile(FileInfo file)
+		{
+			if(file.Name.Equals(R.strings.SKIP_DESKTOP_INI) ||
+			        file.Name.Equals(R.strings.LOG_FILE_NAME))
+				return FileVisitResult.Continue;
+			
+			STATS.Count(FileEvent.FileFound);
+			
+			Suggestions.Clear();
+			progressWorker.Task.VisitFile(Suggestions, file);
+			
+			return FileVisitResult.Continue;
+		}
+		
+		public override FileVisitResult VisitFileFailed(FileSystemInfo file, IOException exc)
+		{			
+			LOG.ErrorFormat("{0}: {1}", file.Name, exc.ToString());
+			STATS.Count(FileEvent.FileError);
+			
+			progressWorker.Task.VisitFileFailed(Suggestions, file, exc);
+			
+			return FileVisitResult.Continue;
+		}
 	}
 }
