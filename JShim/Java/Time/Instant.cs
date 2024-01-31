c72a84573c2e890599ef6d7e1ec7ba450ef6e299
gyk <147011991+gyk4j@users.noreply.github.com>
2024-01-31 15:44:06 +0800

    Add java.time.Instant (#132)

---
+++ JShim/Java/Time/Instant.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Globalization;
+
+namespace Java.Time
+{
+	/// <summary>
+	/// Description of Instant.
+	/// </summary>
+	public class Instant
+	{
+		public static DateTime Parse(string s)
+		{
+			return DateTime.Parse(s, null, DateTimeStyles.RoundtripKind);
+		}
+	}
+}
