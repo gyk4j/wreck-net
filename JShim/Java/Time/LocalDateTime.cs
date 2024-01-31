3d253146ca0ff0f2d18bd0ba3399cceb7c4d8ebb
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-01-31 16:10:33 +0800

    Add java.time.LocalDateTime

---
+++ JShim/Java/Time/LocalDateTime.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.Globalization;
+
+using log4net;
+
+namespace Java.Time
+{
+	/// <summary>
+	/// Description of LocalDateTime.
+	/// </summary>
+	public sealed class LocalDateTime
+	{
+		private static readonly ILog LOG = LogManager.GetLogger(typeof(LocalDateTime));
+		
+		public static DateTime Parse(string s, string dateTimeFormatter)
+		{
+			DateTime l = DateTime.MaxValue;
+			bool isParsed = DateTime.TryParseExact(
+					s,
+					dateTimeFormatter,
+					CultureInfo.InvariantCulture,
+					DateTimeStyles.RoundtripKind,
+					out l);
+			
+			if(!isParsed)
+				LOG.WarnFormat("Parse failed: {0}", s);
+			
+			//Debug.Assert(!l.Equals(DateTime.MaxValue));
+			
+			return l;
+		}
+	}
+}
