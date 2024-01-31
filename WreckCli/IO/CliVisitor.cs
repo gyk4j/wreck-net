d6f3c0b74c207baf188d716ec3bd1166138e7f29
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-01-31 15:03:05 +0800

    Add abstract file visitor

--- WreckCli/IO/CliVisitor.cs
+++ WreckCli/IO/CliVisitor.cs
@@ -14,34 +14,21 @@ namespace Wreck.IO
 	/// <summary>
 	/// Description of CliVisitor.
 	/// </summary>
-	public class CliVisitor : SimpleFileVisitor
+	public class CliVisitor : AbstractFileVisitor
 	{
-		private static readonly ILog LOG = LogManager.GetLogger(typeof(CliVisitor));
+		private static readonly ILog LOG = LogManager.GetLogger(typeof(AbstractFileVisitor));
 		private static readonly StatisticsCollector STATS = StatisticsCollector.Instance;
 		
 		CliWorker progressWorker = null;
 		
-		private readonly List<DirectoryInfo> directories;
-		private readonly Dictionary<CorrectionEnum, DateTime> suggestions;
-		
 		public CliVisitor(CliWorker progressWorker) : base()
 		{
 			this.progressWorker = progressWorker;
-			this.directories = new List<DirectoryInfo>();
-			this.suggestions = new Dictionary<CorrectionEnum, DateTime>();
 		}
-		
-		public List<DirectoryInfo> Directories { get { return directories; } }
-		public Dictionary<CorrectionEnum, DateTime> Suggestions { get { return suggestions; } }
 	
 		public override FileVisitResult PreVisitDirectory(DirectoryInfo dir)
 		{
-			STATS.Count(FileEvent.DirectoryFound);
-			STATS.Count(FileEvent.FileFound);
-			
-			Directories.Add(dir);
-			
-			Suggestions.Clear();
+			base.PreVisitDirectory(dir);
 			progressWorker.Task.PreVisitDirectory(Suggestions, dir);
 			
 			return FileVisitResult.Continue;
@@ -49,18 +36,7 @@ namespace Wreck.IO
 		
 		public override FileVisitResult PostVisitDirectory(DirectoryInfo dir, IOException exc)
 		{			
-			if(exc != null)
-			{
-				LOG.Error(exc.ToString());
-				return FileVisitResult.Continue;
-			}
-			
-			bool ok = Directories.Remove(dir);
-			
-			if(!ok)
-				LOG.WarnFormat("{0}: attrs = null", dir.FullName);
-			
-			Suggestions.Clear();
+			base.PostVisitDirectory(dir, exc);
 			progressWorker.Task.PostVisitDirectory(Suggestions, dir);
 			
 			return FileVisitResult.Continue;
@@ -68,13 +44,7 @@ namespace Wreck.IO
 		
 		public override FileVisitResult VisitFile(FileInfo file)
 		{
-			if(file.Name.Equals(R.strings.SKIP_DESKTOP_INI) ||
-			        file.Name.Equals(R.strings.LOG_FILE_NAME))
-				return FileVisitResult.Continue;
-			
-			STATS.Count(FileEvent.FileFound);
-			
-			Suggestions.Clear();
+			base.VisitFile(file);
 			progressWorker.Task.VisitFile(Suggestions, file);
 			
 			return FileVisitResult.Continue;
@@ -82,9 +52,7 @@ namespace Wreck.IO
 		
 		public override FileVisitResult VisitFileFailed(FileSystemInfo file, IOException exc)
 		{			
-			LOG.ErrorFormat("{0}: {1}", file.Name, exc.ToString());
-			STATS.Count(FileEvent.FileError);
-			
+			base.VisitFileFailed(file, exc);
 			progressWorker.Task.VisitFileFailed(Suggestions, file, exc);
 			
 			return FileVisitResult.Continue;
