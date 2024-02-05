9179469e8160fd459345a42890eafaf021ae6165
gyk <147011991+gyk4j@users.noreply.github.com>
2024-02-05 15:01:42 +0800

    Add entity beans (#152)

---
+++ Wreck/Entity/FileStatisticsBean.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace Wreck.Entity
+{
+	/// <summary>
+	/// Description of FileStatisticsBean.
+	/// </summary>
+	public class FileStatisticsBean
+	{
+		private readonly string statistic;
+		private readonly int count;
+		
+		public FileStatisticsBean(string statistic, int count) : base()
+		{
+			this.statistic = statistic;
+			this.count = count;
+		}
+		
+		public string Statistic
+		{
+			get { return statistic; }
+		}
+
+		public int Count
+		{
+			get { return count; }
+		}
+	}
+}
---
+++ Wreck/Entity/MetadataBean.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Java.Time;
+
+namespace Wreck.Entity
+{
+	/// <summary>
+	/// Description of MetadataBean.
+	/// </summary>
+	public class MetadataBean
+	{
+		private readonly IDictionary<string, Instant> tags = new Dictionary<string, Instant>();
+		
+		public IDictionary<string, Instant> Tags
+		{
+			get { return tags; }
+		}
+		
+		public void AddTag(string key, Instant value)
+		{
+			if(Tags.ContainsKey(key))
+				Tags[key] = value;
+			else
+				Tags.Add(key, value);
+		}
+		
+		public Instant Earliest
+		{
+			get
+			{
+				Instant i = null;
+				foreach(Instant v in Tags.Values)
+				{
+					if(i == null || (v != null && v.DateTime.CompareTo(i.DateTime) < 0))
+						i = v;
+						
+				}
+				return i;
+			}
+		}
+		
+		public Instant Latest
+		{
+			get
+			{
+				Instant i = null;
+				foreach(Instant v in Tags.Values)
+				{
+					if(i == null || (v != null && v.DateTime.CompareTo(i.DateTime) > 0))
+						i = v;
+				}
+				return i;
+			}
+		}
+	}
+}
