88811e846d167fc466f0d010fe7c4f144182ea90
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-02-16 16:24:16 +0800

    Fix type generics, length property and misssing streams

---
+++ JShim/Java/IO/ObjectOutputStream.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Java.IO
+{
+	/// <summary>
+	/// Description of ObjectOutputStream.
+	/// </summary>
+	public class ObjectOutputStream
+	{
+		public void DefaultWriteObject()
+		{
+			
+		}
+		
+		public void WriteObject(object o)
+		{
+			Stream s = new MemoryStream();
+			BinaryFormatter formatter = new BinaryFormatter();
+			formatter.Serialize(s, o);
+			s.Close();
+		}
+	}
+}
--- JShim/Javax/Swing/Event/ChangeListener.cs
+++ JShim/Javax/Swing/Event/ChangeListener.cs
@@ -1,12 +1,13 @@
 ﻿
 using System;
+using Java.Util;
 
 namespace Javax.Swing.Event
 {
 	/// <summary>
 	/// Defines an object which listens for ChangeEvents.
 	/// </summary>
-	public interface ChangeListener
+	public interface ChangeListener : EventListener
 	{
 		/// <summary>
 		/// Invoked when the target of the listener has changed its state.
