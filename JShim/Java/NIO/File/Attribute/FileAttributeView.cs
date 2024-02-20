c35f092b500d87491d39d6f29d416c3d122aa306
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-02-20 11:59:04 +0800

    Add AttributeView and FileAttributeView interface

---
+++ JShim/Java/NIO/File/Attribute/FileAttributeView.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+namespace Java.NIO.File.Attribute
+{
+	/// <summary>
+	/// An attribute view that is a read-only or updatable view of non-opaque 
+	/// values associated with a file in a filesystem. This interface is 
+	/// extended or implemented by specific file attribute views that define 
+	/// methods to read and/or update the attributes of a file.
+	/// </summary>
+	public interface FileAttributeView : AttributeView
+	{
+	}
+}
