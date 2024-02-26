d7eb12c3290b631ea2c61115e5c32aad7daf3991
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-02-26 20:42:35 +0800

    Add StandardCopyOption enum

---
+++ JShim/Java/NIO/File/StandardCopyOption.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Java.NIO.File
+{
+	/// <summary>
+	/// Description of StandardCopyOption.
+	/// </summary>
+	public enum StandardCopyOption
+	{
+		/// <summary>
+		/// Move the file as an atomic file system operation.
+		/// </summary>
+		AtomicMove,
+		
+		/// <summary>
+		/// Copy attributes to the new file.
+		/// </summary>
+		CopyAttributes,
+		
+		/// <summary>
+		/// Replace an existing file if it exists.
+		/// </summary>
+		ReplaceExisting
+	}
+}
