d816c04b4b1a24cd4c5ac1a9de1f7d251b236a9e
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-02-26 20:55:50 +0800

    Add AccessMode enum

---
+++ JShim/Java/NIO/File/AccessMode.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace Java.NIO.File
+{
+	/// <summary>
+	/// Defines access modes used to test the accessibility of a file.
+	/// </summary>
+	public enum AccessMode
+	{
+		/// <summary>
+		/// Test execute access.
+		/// </summary>
+		Execute,
+		
+		/// <summary>
+		/// Test read access.
+		/// </summary>
+		Read,
+		
+		/// <summary>
+		/// Test write access.
+		/// </summary>
+		Write
+	}
+}
