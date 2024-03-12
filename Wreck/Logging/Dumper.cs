97547cae4bcd92603cf365988230b8bf7a9440f5
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-03-12 08:31:02 +0800

    Move tracing to Dumper

---
+++ Wreck/Logging/Dumper.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using log4net;
+
+namespace Wreck.Logging
+{
+	/// <summary>
+	/// Description of Dumper.
+	/// </summary>
+	public static class Dumper
+	{
+		private static readonly ILog LOG = LogManager.GetLogger(typeof(Dumper));
+		
+		public static void Dump<K,V>(IDictionary<K, V> values)
+		{
+			foreach(KeyValuePair<K, V> entry in values)
+			{
+				LOG.DebugFormat(
+					"{0} = {1}",
+					entry.Key.ToString(),
+					entry.Value.ToString());
+			}
+		}
+	}
+}
