0eba0ee03004c51764b3c8b23b45e57462e70969
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-03-13 14:57:18 +0800

    Fix local UTC time comparison bug

--- JShim/Java/NIO/File/Attribute/BasicFileAttributeView.cs
+++ JShim/Java/NIO/File/Attribute/BasicFileAttributeView.cs
@@ -1,22 +1,24 @@
 ﻿
 using System;
 using System.IO;
+using System.Text;
+
 using Java.Time;
 using log4net;
 
 namespace Java.NIO.File.Attribute
 {
 	/// <summary>
-	/// A file attribute view that provides a view of a basic set of file 
-	/// attributes common to many file systems. The basic set of file 
-	/// attributes consist of mandatory and optional file attributes as defined 
+	/// A file attribute view that provides a view of a basic set of file
+	/// attributes common to many file systems. The basic set of file
+	/// attributes consist of mandatory and optional file attributes as defined
 	/// by the BasicFileAttributes interface.
 	/// 
-	/// The file attributes are retrieved from the file system as a bulk 
-	/// operation by invoking the readAttributes method. This class also 
+	/// The file attributes are retrieved from the file system as a bulk
+	/// operation by invoking the readAttributes method. This class also
 	/// defines the setTimes method to update the file's time attributes.
 	/// 
-	/// Where dynamic access to file attributes is required, the attributes 
+	/// Where dynamic access to file attributes is required, the attributes
 	/// supported by this attribute view have the following names and types:
 	/// <list type="table">
 	/// 	<listheader>
@@ -62,13 +64,13 @@ namespace Java.NIO.File.Attribute
 	/// </list>
 	/// 
 	/// <para>
-	/// The <c>GetAttribute</c> method may be used to read any of these 
+	/// The <c>GetAttribute</c> method may be used to read any of these
 	/// attributes as if by invoking the <c>ReadAttributes()</c> method.
 	/// </para>
 	/// 
 	/// <para>
-	/// The <c>SetAttribute</c> method may be used to update the file's last 
-	/// modified time, last access time or create time attributes as if by 
+	/// The <c>SetAttribute</c> method may be used to update the file's last
+	/// modified time, last access time or create time attributes as if by
 	/// invoking the <c>SetTimes</c> method.
 	/// </para>
 	/// </summary>
@@ -89,26 +91,79 @@ namespace Java.NIO.File.Attribute
 		{
 			bool readOnly = DisableWriteProtection();
 			
-			if(creation.HasValue)
-				file.CreationTime = creation.Value;
+			StringBuilder sb = new StringBuilder();
 			
-			if(modified.HasValue)
-				file.LastWriteTime = modified.Value;
+			if(creation.HasValue &&
+			   (
+			   	(creation.Value.Kind == DateTimeKind.Local &&
+			   	 !file.CreationTime.Equals(creation.Value)) ||
+			   	(creation.Value.Kind == DateTimeKind.Utc &&
+			   	 !file.CreationTimeUtc.Equals(creation.Value))
+			   )
+			  )
+			{
+				if(creation.Value.Kind == DateTimeKind.Local)
+					file.CreationTime = creation.Value;
+				else if(creation.Value.Kind == DateTimeKind.Utc)
+					file.CreationTimeUtc = creation.Value;
+				
+				sb.Append(Instant.From(creation.Value).ToString());
+			}
+			else
+				sb.Append("-");
 			
-			if(accessed.HasValue)
-				file.LastAccessTime = accessed.Value;
+			sb.Append(", ");
+			
+			if(modified.HasValue &&
+			   (
+			   	(modified.Value.Kind == DateTimeKind.Local &&
+			   	 !file.LastWriteTime.Equals(modified.Value)
+			   	) ||
+			   	(modified.Value.Kind == DateTimeKind.Utc &&
+			   	 !file.LastWriteTimeUtc.Equals(modified.Value)
+			   	)
+			   )
+			  )
+			{
+				if(modified.Value.Kind == DateTimeKind.Local)
+					file.LastWriteTime = modified.Value;
+				else if(modified.Value.Kind == DateTimeKind.Utc)
+					file.LastWriteTimeUtc = modified.Value;
+				
+				sb.Append(Instant.From(modified.Value).ToString());
+			}
+			else
+				sb.Append("-");
+			
+			sb.Append(", ");
+			
+			if(accessed.HasValue &&
+			   (
+			   	(accessed.Value.Kind == DateTimeKind.Local && 
+			   	 !file.LastAccessTime.Equals(accessed.Value)) ||
+			   	(accessed.Value.Kind == DateTimeKind.Utc &&
+			   	!file.LastAccessTimeUtc.Equals(accessed.Value))
+			   )
+			  )
+			{
+				if(accessed.Value.Kind == DateTimeKind.Local)
+					file.LastAccessTime = accessed.Value;
+				else if(accessed.Value.Kind == DateTimeKind.Utc)
+					file.LastWriteTimeUtc = accessed.Value;
+				
+				sb.Append(Instant.From(accessed.Value).ToString());
+			}
+			else
+				sb.Append("-");
 			
 			EnableWriteProtection(readOnly);
 			
-			bool changed = creation.HasValue || modified.HasValue || accessed.HasValue;
-			if(changed)
-			{
-				LOG.InfoFormat("Updated {0}: {1}, {2}, {3}",
-				               file.FullName,
-				               (creation.HasValue)? Instant.From(creation.Value).ToString() : "-",
-				               (modified.HasValue)? Instant.From(modified.Value).ToString() : "-",
-				               (accessed.HasValue)? Instant.From(accessed.Value).ToString() : "-");
-			}
+			string change = sb.ToString();
+			
+			if(!"-, -, -".Equals(change)) // "-, -, -" = no change.
+				LOG.DebugFormat("{0}: {1}", file.FullName, change);
+			else
+				LOG.DebugFormat("Unchanged: {0}", file.FullName);
 		}
 		
 		private bool DisableWriteProtection()
